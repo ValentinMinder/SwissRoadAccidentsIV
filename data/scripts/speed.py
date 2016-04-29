959ae3451e05900d7be47334d1f0403b37979323
fa18swiss <fa18swiss@users.noreply.github.com>
2016-04-29 09:57:27 +0200

    rewriting sign logic

--- data/scripts/speed.py
+++ data/scripts/speed.py
@@ -1,19 +1,38 @@
 from codecs import open
 import json
-
 from collections import OrderedDict
 
+def in_town(year):
+    if year > 1983:
+        return "50"
+    return "unlimited"
+
+def out_town(year):
+    if year > 1988:
+        return "80"
+    return "unlimited"
+
+def small_highway(year):
+    if year > 1988:
+        return "100"
+    return "unlimited"
+
+def highway(year):
+    if year > 1988:
+        return "120"
+    return "unlimited"
+
+
 data = OrderedDict()
-with open("../speed.csv", "r", encoding="utf-8") as f:
-    next(f)
-    for l in f:
-        p = l.strip().split(";")
-        data[p[0]] = {
-            "in_town": p[1],
-            "out_town": p[2],
-            "small_highway": p[3],
-            "highway": p[4],
-        }
+
+for year in range(1970, 2016+1):
+    data[year] = {
+        "in_town": in_town(year),
+        "out_town": out_town(year),
+        "small_highway": small_highway(year),
+        "highway": highway(year),
+    }
 
 with open("../speed.json", "w", encoding="utf-8") as f:
     json.dump(data, f, indent=True)
+
--- js/application.js
+++ js/application.js
@@ -1,6 +1,15 @@
 $(document).ready(function() {
     $("#msgid").html("This is Hello World by JQuery");
     $("#range").rangeYear();
-    $("#sign").speedSign();
+    $(document).speedSign({
+        in_town_sign: "#sign div:nth-child(1)",
+        in_town_speed: "#sign div:nth-child(2)",
+        out_town_sign: "#sign div:nth-child(3)",
+        out_town_speed: "#sign div:nth-child(4)",
+        small_highway_sign: "#sign div:nth-child(5)",
+        small_highway_speed: "#sign div:nth-child(6)",
+        highway_sign: "#sign div:nth-child(7)",
+        highway_speed: "#sign div:nth-child(8)",
+    });
     $("#alcool").alcoolSign();
 });
