adc1a01b7d588ccf6ee63613ac1e6ec7b280d71d
fa18swiss <fa18swiss@users.noreply.github.com>
2016-05-12 15:22:33 +0200

    add mustache

--- js/range-year.js
+++ js/range-year.js
@@ -1,15 +1,28 @@
 (function( $ ) {
     $.fn.rangeYear = function() {
         var that = this;
-        $.getValues("settings", function(data) {
-            var input = $('<input type="range" value="'+(data.year_to + data.year_from)/2+'" min="'+data.year_from+'" max="'+data.year_to+'"class="form-control"/>');
-            that.html(input);
-            input.change(function() {
-                $(document).trigger("year-change",  $(this).val());
-            });
-            setTimeout(function() {
-                input.change();
-            }, 100);
+        var template = null;
+        var data = null;
+        $.getValues("settings", function(d) {
+            data = d;
+            createRange(that, data, template);
         });
+        $.get("templates/range-year.html", function(t) {
+            template = t;
+            createRange(that, data, template);
+        })
     };
+    function createRange(that, data, template) {
+        if (!data) return;
+        if (!template) return;
+        var input = $(Mustache.render(template, data));
+        input.val((data.year_from + data.year_to) / 2);
+        that.html(input);
+        input.change(function() {
+            $(document).trigger("year-change",  $(this).val());
+        });
+        setTimeout(function() {
+            input.change();
+        }, 100);
+    }
 }( jQuery ));
