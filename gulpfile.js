68b5b951c5a26daa804c235a65927fa9782f0d6e
fa18swiss <fa18swiss@users.noreply.github.com>
2016-05-12 17:37:34 +0200

    add gult
    Use gult for less and min css
    use standard license name

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require("gulp");
+var less = require("gulp-less");
+var path = require("path");
+var cleanCSS = require("gulp-clean-css");
+var rename = require("gulp-rename");
+
+gulp.task("default", [
+    "less",
+]);
+
+gulp.task("min", [
+    "min/css",
+]);
+
+gulp.task("less", function () {
+    return gulp.src("./css/style.less")
+        .pipe(less())
+        .pipe(gulp.dest("./css/"));
+});
+gulp.task("min/css", ["less"], function () {
+    return gulp.src("./css/style.css")
+        .pipe(cleanCSS())
+        .pipe(gulp.dest("./css/"));
+});
