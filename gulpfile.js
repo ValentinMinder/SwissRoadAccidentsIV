bd10728ebe55cd054938bd3af9b8cc820e847f2f
fa18swiss <fa18swiss@users.noreply.github.com>
2016-06-02 14:13:57 +0200

    create build gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 var gulp = require("gulp");
-var less = require("gulp-less");
-var path = require("path");
+var clean = require('gulp-clean');
 var cleanCSS = require("gulp-clean-css");
 var exec = require('gulp-exec');
+var ghtmlSrc = require('gulp-html-src');
+var htmlmin = require('gulp-htmlmin');
+var jsonmin = require('gulp-jsonmin');
+var less = require("gulp-less");
+var runSequence = require('run-sequence');
+var svgmin = require('gulp-svgmin');
+var uglify = require('gulp-uglify');
 
 gulp.task("default", [
     "less",
-]);
-
-gulp.task("min", [
-    "min/css",
 ]);
 
 gulp.task("less", function () {
@@ -17,11 +19,7 @@ gulp.task("less", function () {
         .pipe(less())
         .pipe(gulp.dest("./css/"));
 });
-gulp.task("min/css", ["less"], function () {
-    return gulp.src("./css/style.css")
-        .pipe(cleanCSS())
-        .pipe(gulp.dest("./css/"));
-});
+
 
 gulp.task('watch', function() {
     gulp.watch('./css/*.less', ['less']); 
@@ -42,4 +40,100 @@ gulp.task("data", function () {
     gulp.src('./data/scripts/*.py')
         .pipe(exec('py -3 <%= file.path %>', options))
         .pipe(exec.reporter(reportOptions));
+});
+
+gulp.task("min/js", function() {
+    return gulp.src('./js/*.js')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./min/js/'));
+});
+
+gulp.task('clean', function() {
+    return gulp.src('./build', {read: false})
+        .pipe(clean());
+});
+
+gulp.task("copy", [
+    "copy/css",
+    "copy/html",
+    "copy/js",
+    "copy/json",
+    "copy/svg",
+    "copy/templates",
+]);
+
+gulp.task('copy/css', ["less"], function() {
+    gulp.src('./*.html')
+        .pipe(ghtmlSrc({ presets: 'css'}))
+        .pipe(gulp.dest('./build/copy'));
+});
+gulp.task('copy/html', function() {
+    gulp.src('./*.html')
+        .pipe(gulp.dest('./build/copy'));
+});
+gulp.task('copy/js', function() {
+    gulp.src('./*.html')
+        .pipe(ghtmlSrc())
+        .pipe(gulp.dest('./build/copy'));
+});
+gulp.task("copy/json", function() {
+    return gulp.src('./data/**/*.json')
+        .pipe(gulp.dest('./build/copy/data'));
+});
+gulp.task("copy/svg", function() {
+    return gulp.src('./img/**/*.svg')
+        .pipe(gulp.dest('./build/copy/img'));
+});
+gulp.task('copy/templates', function() {
+    return gulp.src('./templates/*.html')
+        .pipe(gulp.dest('./build/copy/templates/'));
+});
+
+gulp.task("min", [
+    "min/css",
+    "min/html",
+    "min/js",
+    "min/json",
+    "min/svg",
+]);
+gulp.task("min/css", function () {
+    return gulp.src("./build/copy/**/*.css")
+        .pipe(cleanCSS())
+        .pipe(gulp.dest("./build/min"));
+});
+gulp.task("min/html", function() {
+    return gulp.src("./build/copy/**/*.html")
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest("./build/min"));
+});
+gulp.task("min/js", [
+    "min/js/bower",
+    "min/js/app",
+]);
+gulp.task("min/js/bower", function() {
+    return gulp.src("./build/copy/bower_components/**/*.js")
+        .pipe(gulp.dest("./build/min/bower_components"));
+});
+gulp.task("min/js/app", function() {
+    return gulp.src("./build/copy/js/*.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("./build/min/js/"));
+});
+gulp.task("min/json", function() {
+    return gulp.src("./build/copy/**/*.json")
+        .pipe(jsonmin())
+        .pipe(gulp.dest("./build/min"));
+});
+gulp.task("min/svg", function() {
+    return gulp.src("./build/copy/**/*.svg")
+        .pipe(svgmin())
+        .pipe(gulp.dest("./build/min"));
+});
+
+gulp.task('build', function(callback) {
+    runSequence(
+        'clean',
+        'copy',
+        'min',
+        callback);
 });
